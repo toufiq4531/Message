eeb4f217fdc93588a66b6005c638d7325c24c9a4
Mohammad Tofiqul Islam <118299820+toufiq4531@users.noreply.github.com>
2024-10-22 17:26:46 +0600

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "Amio tomake onek beshi Bhalobashi";
+  question.innerHTML = "Awwww!! Thank you👉🏻👈🏻";
   gif.src =
     "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
 });
